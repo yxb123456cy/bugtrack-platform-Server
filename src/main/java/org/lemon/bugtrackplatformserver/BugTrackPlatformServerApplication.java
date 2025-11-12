7c034e67881ff94d3a2d2a266e6daefb746d8026
小橘子 <12267938+xiaoye123cy@user.noreply.gitee.com>
2025-11-13 01:09:12 +0800

    feat: :sparkles: 初始化JDK21+SpringBoot3项目

---
+++ src/main/java/org/lemon/bugtrackplatformserver/BugTrackPlatformServerApplication.java
@@ -0,0 +1,13 @@
+package org.lemon.bugtrackplatformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackPlatformServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackPlatformServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lemon/bugtrackplatformserver/BugtrackPlatformServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lemon.bugtrackplatformserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugtrackPlatformServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
